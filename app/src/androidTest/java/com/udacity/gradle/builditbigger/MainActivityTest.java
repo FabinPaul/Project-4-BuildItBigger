2fa599b1ea92161b79a730bb9426ead20405cd8b
Fabin Paul <fabinpaul92@gmail.com>
2017-03-10 17:49:11 +0530

    added test case for InterstitialAd

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -10,6 +11,8 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
@@ -27,6 +30,12 @@ public class MainActivityTest {
     @Test
     public void testIfJokeDisplayed() throws Throwable {
         onView(withText(mActivityRule.getActivity().getString(R.string.button_text))).perform(click());
+        try {
+            onView(withContentDescription("Interstitial close button")).check(matches(isDisplayed()));
+            onView(withContentDescription("Interstitial close button")).perform(click());
+        }catch (NoMatchingViewException e) {
+
+        }
         onView(withId(R.id.jokes_txtvw)).check(matches(withText(not(isEmptyOrNullString()))));
         onView(withId(R.id.jokes_txtvw)).check(matches(withText(not(mActivityRule.getActivity().getString(R.string.no_jokes_avail)))));
     }
